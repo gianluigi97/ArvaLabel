4e230418175673ea3d688197672c1dc6188efd07
gianluigi97 <gianluigi.mosti97@gmail.com>
2025-07-09 13:53:54 +0000

    modifica layout navbar

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8000)
